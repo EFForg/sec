4a3bd364b09f9cd10e6cded96f8379e1b6a93391
vivian <vivian@eff.org>
2017-10-02 14:13:38 -0700

    Install activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 end
