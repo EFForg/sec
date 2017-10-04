b3410334e0ad015e46269133bd50221dcf129176
vivian <vivian@eff.org>
2017-10-04 13:24:36 -0700

    Parse duration hours and minutes

--- app/admin/topic.rb
+++ app/admin/topic.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Topic do
-  permit_params :name, lessons_attributes: [:id, :name, :duration, :body, :topic_id, :_destroy]
+  permit_params :name, lessons_attributes: [:id, :name, :duration_hours, :duration_minutes, :body, :topic_id, :_destroy]
 
   form do |f|
     f.inputs do
@@ -8,16 +8,18 @@ ActiveAdmin.register Topic do
         topic.lessons.each do |lesson|
           tab lesson.name do
             f.fields_for :lessons, lesson do |l|
-              l.input :duration
+              l.input :duration_hours, as: :number, label: 'Hours'
+              l.input :duration_minutes, as: :number, label: 'Minutes'
               l.input :body, as: :ckeditor
-              l.input :_destroy, as: :boolean, label: "Delete this level"
+              l.input :_destroy, as: :boolean, label: 'Delete this level'
             end
           end
         end
         tab '+Add' do
           f.fields_for :lessons, topic.lessons.new do |l|
             l.input :name
-            l.input :duration
+            l.input :duration_hours, as: :number, label: 'Hours'
+            l.input :duration_minutes, as: :number, label: 'Minutes'
             l.input :body, as: :ckeditor
           end
         end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,31 @@
 class Lesson < ApplicationRecord
   belongs_to :topic
+  before_save :set_duration
+
+  def set_duration
+    if @duration_hours.present? or @duration_minuts.present?
+      self.duration = @duration_hours.hours.to_i + @duration_minutes.minutes.to_i
+    end
+  end
+
+  def duration_hours
+    (duration/3600).floor if duration.present? and duration > 0
+  end
+
+  def duration_minutes
+    if duration.present? and duration > 0
+      minutes = (duration % 3600)/60
+      minutes.round
+    end
+  end
+
+  def duration_hours=(hours)
+    duration_will_change! unless hours == duration_hours
+    @duration_hours = hours.to_f
+  end
+
+  def duration_minutes=(minutes)
+    duration_will_change! unless minutes == duration_minutes
+    @duration_minutes = minutes.to_f
+  end
 end
