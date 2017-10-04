fc7d3f91939d6bee42627a69382c6dbdce01df18
Peter Woo <peterw@eff.org>
2017-10-04 15:25:20 -0700

    Stub out home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def tags(object)
+  end
+
+  def duration(time)
+  end
+
+  def topics_path
+    "/topics"
+  end
+
+  def articles_path
+    "/articles"
+  end
+
+  def materials_path
+    "/materials"
+  end
+
+  def blog_path
+    "/blog"
+  end
 end
---
+++ app/models/homepage.rb
@@ -0,0 +1,58 @@
+# coding: utf-8
+class Homepage # < ApplicationRecord
+  def welcome
+    <<-EOF
+<p>Welcome to the Security Education website!</p>
+
+<p>Here you can find training materials for all your
+security education needs.</p>
+
+<p><a href="#">I’m lost! Take me back to SSD!</a></p>
+
+<p><a href="#">Training modules</a></p>
+
+<p><a href="#">Being a Trainer - Advice</a></p>
+
+<p><a href="#">Blog</a></p>
+
+<p><a href="#">Enrichment Materials</a></p>
+EOF
+  end
+
+  def teaching_advice
+    <<-EOF
+<p>Whether you’re a computer scientist, a community organizer or
+just a curious person, Security Education’s collection of
+resources has something for you.</p>
+EOF
+  end
+
+  def promoted_topics
+    topics = []
+
+    4.times do
+      topics << Topic.new(name: "Title")
+    end
+
+    topics
+  end
+
+  def promoted_articles
+    [
+      OpenStruct.new(name: "Social Media", to_partial_path: "articles/article"),
+      OpenStruct.new(name: "Phishing", to_partial_path: "articles/article"),
+      OpenStruct.new(name: "Passwords", to_partial_path: "articles/article")
+    ]
+  end
+
+  def promoted_materials
+    [
+      OpenStruct.new(name: "Downloadable PDFs and slides", to_partial_path: "materials/material"),
+      OpenStruct.new(name: "Install Tools", to_partial_path: "materials/material")
+    ]
+  end
+
+  def promoted_blog_post
+    OpenStruct.new(name: "Highlighted post", to_partial_path: "blog/blog_post")
+  end
+end
