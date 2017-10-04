7d4ac14258c1ff4c847a79f298c7bac150cbe6ac
Peter Woo <peterw@eff.org>
2017-10-04 15:39:21 -0700

    Migration/controller/routes for articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   root to: "home#index"
 
+  resources :articles, only: [:index, :show]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 end
