ef0360d79c6b0b048ebf17c686baf137529758b8
vivian <vivian@eff.org>
2017-10-04 08:54:45 -0700

    Render edit lessons as tabs

--- app/admin/topic.rb
+++ app/admin/topic.rb
@@ -2,12 +2,25 @@ ActiveAdmin.register Topic do
   permit_params :name, lessons_attributes: [:id, :name, :duration, :body, :topic_id]
 
   form do |f|
-    f.inputs :name
     f.inputs do
-      f.has_many :lessons, heading: 'Lessons',
-                           allow_destroy: true do |a|
-        a.input :name
-        a.input :body, as: :ckeditor
+      f.input :name
+      tabs do
+        topic.lessons.each do |lesson|
+          tab lesson.name do
+            f.fields_for :lessons, lesson do |l|
+              l.input :name
+              l.input :duration
+              l.input :body, as: :ckeditor
+            end
+          end
+        end
+        tab '+Add' do
+          f.fields_for :lessons, topic.lessons.new do |l|
+            l.input :name
+            l.input :duration
+            l.input :body, as: :ckeditor
+          end
+        end
       end
     end
     f.actions
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ApplicationRecord
   has_many :lessons
-  accepts_nested_attributes_for :lessons, allow_destroy: true
+  accepts_nested_attributes_for :lessons, allow_destroy: true, reject_if: :all_blank
 
 end
