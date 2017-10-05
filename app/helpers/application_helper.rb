ee68fb991618fe3e4e71daab44a31bd2e21f59cc
Peter Woo <peterw@eff.org>
2017-10-04 18:10:33 -0700

    Little clean up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,9 @@ module ApplicationHelper
   end
 
   def duration(time)
-    distance_of_time_in_words(60 * 60 * time)
+    if time
+      distance_of_time_in_words(60 * 60 * time)
+    end
   end
 
   def materials_path
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,4 @@
 class Topic < ApplicationRecord
   has_many :lessons
   accepts_nested_attributes_for :lessons, allow_destroy: true
-
 end
