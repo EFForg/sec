cd669b7eb0d7a16cb8c74be568fb32d46fe34f14
Peter Woo <peterw@eff.org>
2017-10-05 12:57:39 -0700

    Move things out of ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,20 +9,6 @@ module ApplicationHelper
     end
   end
 
-  def preview(html)
-    allowed_tags = %w(a b strong i em u s strike del)
-    truncate_html(sanitize(html, tags: allowed_tags), length: 500)
-  end
-
-  def tags(object)
-  end
-
-  def duration(time)
-    if time
-      distance_of_time_in_words(time)
-    end
-  end
-
   def materials_path
     "/materials"
   end
