6b211d863bf7ddbb5a17f27380d49f5c2025cb45
Peter Woo <peterw@eff.org>
2017-10-05 11:32:13 -0700

    Undo accidental comment in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
 welcome = <<-EOF
 <p>Welcome to the Security Education website!</p>
