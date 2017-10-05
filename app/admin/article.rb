0a12545e0f030da7f3a6024815083a5d7087fb7e
Peter Woo <peterw@eff.org>
2017-10-05 11:50:22 -0700

    Admin form for articles

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Article do
   permit_params :name, :body
+
+  form do |f|
+    inputs do
+      f.input :name
+
+      f.input :body, as: :ckeditor
+    end
+
+    f.actions
+  end
 end
