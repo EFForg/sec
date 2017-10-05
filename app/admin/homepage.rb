42ddbcd169825c87ef3b86f2e73c8bd58084aebb
Peter Woo <peterw@eff.org>
2017-10-04 18:10:21 -0700

    Start of admin page for homepage

---
+++ app/admin/homepage.rb
@@ -0,0 +1,44 @@
+ActiveAdmin.register Homepage do
+  permit_params :welcome, :articles_intro,
+    promoted_topic_content_attributes: [
+      :id, :_destroy, :content_type, :content_id_string, :position
+    ],
+    promoted_article_content_attributes: [
+      :id, :_destroy, :content_type, :content_id_string, :position
+    ]
+
+  form do |f|
+    inputs do
+      input :welcome, as: :ckeditor
+      input :articles_intro, as: :ckeditor
+    end
+
+    f.inputs "Promoted Topics" do
+      topic_options = Topic.all.map{ |t| "#{t.name} (##{t.id})" }
+      f.has_many :promoted_topic_content,
+                 new_record: "Add New Promoted Topic",
+                 allow_destroy: true,
+                 sortable: :position do |t|
+        t.input :content_type, as: :hidden,
+                input_html: { value: "Topic" }
+        t.input :content_id_string, as: :datalist,
+                label: "Topic", collection: topic_options
+      end
+    end
+
+    f.inputs "Promoted Articles" do
+      article_options = Article.all.map{ |t| "#{t.name} (##{t.id})" }
+      f.has_many :promoted_article_content,
+                 new_record: "Add New Promoted Article",
+                 allow_destroy: true,
+                 sortable: :position do |t|
+        t.input :content_type, as: :hidden,
+                input_html: { value: "Article" }
+        t.input :content_id_string, as: :datalist,
+                label: "Article", collection: article_options
+      end
+    end
+
+    f.actions
+  end
+end
