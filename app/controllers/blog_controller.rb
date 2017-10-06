b6408cd2b8cef4fdf204a6135bebd1b581addc8c
Peter Woo <peterw@eff.org>
2017-10-05 17:12:39 -0700

    Index/show actions for blog

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,2 +1,13 @@
 class BlogController < ApplicationController
+  breadcrumbs "Security Education" => routes.root_path,
+              "Blog" => routes.blog_path
+
+  def index
+    @blog_posts = BlogPost.all.page(params[:page])
+  end
+
+  def show
+    @blog_post = BlogPost.find(params[:id])
+    breadcrumbs @blog_post.name
+  end
 end
