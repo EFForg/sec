8a377e5538f907a8e5a7e09f31b6c343578c07e7
Peter Woo <peterw@eff.org>
2017-10-11 11:39:28 -0700

    Make Lesson.unused_levels an association method

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,12 +1,16 @@
 class Topic < ApplicationRecord
-  has_many :lessons
+  has_many :lessons do
+    def unused_levels
+      unused_levels = Lesson::LEVELS.keys - pluck(:level_id)
+      Lesson::LEVELS.slice(*unused_levels).invert
+    end
+  end
+
   accepts_nested_attributes_for :lessons, allow_destroy: true, reject_if: :all_blank
 
   extend FriendlyId
   friendly_id :name, use: [:slugged, :history]
   before_validation :nillify_empty_slug, prepend: true
-
-
   def unsaved_or_new_lesson
     lessons.find(&:new_record?) || lessons.new
   end
