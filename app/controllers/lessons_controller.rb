f27ffc1b5b8583e87457705845e5155b7a73f515
vivian <vivian@eff.org>
2017-10-11 10:45:39 -0700

    Don't include base lesson in path

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,10 +2,10 @@ class LessonsController < ApplicationController
   def show
     @topic = Topic.friendly.find(params[:topic_id])
     @lesson = @topic.lessons.with_level(params[:id]).take
-    if @lesson
-      render "topics/show"
+    if @lesson.nil? or @lesson.level_id == 0
+      redirect_to @topic
     else
-      redirect_to @topic unless @lesson
+      render "topics/show"
     end
   end
 end
