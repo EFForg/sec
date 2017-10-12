64267bc45ac08cd03e2bc261830a6ee9595388e4
Peter Woo <peterw@eff.org>
2017-10-12 15:44:01 -0700

    Allow more tags in lesson body

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -1,10 +1,15 @@
 module ContentHelper
+  def allowed_tags_for_preview
+    %w(a b strong i em u s strike del p)
+  end
+
   def allowed_tags
-    allowed_tags = %w(a b strong i em u s strike del p)
+    other_tags = %w(h2 h3 h4 ul ol li img blockquote code pre)
+    allowed_tags_for_preview + other_tags
   end
 
   def preview(html)
-    html = truncate(sanitize(html, tags: allowed_tags),
+    html = truncate(sanitize(html, tags: allowed_tags_for_preview),
                     length: 500, escape: false)
 
     Nokogiri::HTML.fragment(html).to_html.html_safe # rubocop:disable Rails/OutputSafety
