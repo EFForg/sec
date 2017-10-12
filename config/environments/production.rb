57f625650730b46ecfab2a8344603365d46b24f1
Peter Woo <peterw@eff.org>
2017-10-12 13:42:13 -0700

    Use redis for caching in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,9 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  if ENV["REDIS_URI"]
+    config.cache_store = :redis_store, ENV["REDIS_URI"]
+  end
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
