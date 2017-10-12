96b88e0fde6754da3ba7a4a9eb4b6307671147b1
Peter Woo <peterw@eff.org>
2017-10-12 15:49:59 -0700

    Don't sanitize lesson body for now

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -3,11 +3,6 @@ module ContentHelper
     %w(a b strong i em u s strike del p)
   end
 
-  def allowed_tags
-    other_tags = %w(h2 h3 h4 ul ol li img blockquote code pre)
-    allowed_tags_for_preview + other_tags
-  end
-
   def preview(html)
     html = truncate(sanitize(html, tags: allowed_tags_for_preview),
                     length: 500, escape: false)
