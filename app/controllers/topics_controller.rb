9c673fa5fb33b3377d377620e2fb751e617e2ca5
Peter Woo <peterw@eff.org>
2017-10-12 17:13:48 -0700

    Add tag navigation to topics#index

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,10 @@
 class TopicsController < ApplicationController
   def index
     @title = "Lesson Topics"
-    @topics = Topic.published.order(published_at: :desc)
+    @topics = topics_scope.published.order(published_at: :desc)
+    @tags = ActsAsTaggableOn::Tag.joins(:taggings).
+            where(taggings: { taggable_type: "Topic" }).
+            distinct
   end
 
   def show
@@ -11,6 +14,16 @@ class TopicsController < ApplicationController
     respond_to do |format|
       format.html
       format.js { render "lessons/show" }
+    end
+  end
+
+  private
+
+  def topics_scope
+    if params[:tag]
+      Topic.tagged_with(params[:tag])
+    else
+      Topic.all
     end
   end
 end
