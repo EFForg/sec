69e85628febcca46ea4a6bc11af3c88cc799bfa9
Peter Woo <peterw@eff.org>
2017-10-13 12:26:41 -0700

    Only watch project directories

--- config/spring.rb
+++ config/spring.rb
@@ -1,3 +1,10 @@
+Spring::Watcher::Listen.class_eval do
+  def base_directories
+    %w(app config lib spec vendor)
+      .uniq.map { |path| Pathname.new(File.join(root, path)) }
+  end
+end
+
 %w(
   .ruby-version
   .rbenv-vars
