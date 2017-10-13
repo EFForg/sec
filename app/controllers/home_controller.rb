274fc2c65801f8a614c43990fdef483936471c41
Peter Woo <peterw@eff.org>
2017-10-13 15:34:52 -0700

    Improve caching on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,4 @@
 class HomeController < ApplicationController
   def index
-    @homepage = Homepage.
-                preload(
-                  featured_articles: :tags,
-                  featured_topics: [:lessons, :tags],
-                  featured_blog_posts: :tags
-                ).
-                take
   end
 end
