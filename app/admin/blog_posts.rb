04a2c33e2470afa32d68053c00876b1958587fab
Peter Woo <peterw@eff.org>
2017-10-16 17:18:13 -0700

    build me

--- app/admin/blog_posts.rb
+++ app/admin/blog_posts.rb
@@ -6,6 +6,7 @@ ActiveAdmin.register BlogPost do
   permit_params :name, :body, :slug, :published, tag_ids: []
 
 
+
   controller do
     def find_resource
       scoped_collection.friendly.find(params[:id])
