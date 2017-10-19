18fd9a1b088ea4b73e9e8abfce657248495ba2e9
Peter Woo <peterw@eff.org>
2017-10-18 17:47:33 -0700

    Fix travis build

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def index
     @results = PgSearch.multisearch(params[:q]).page(params[:page])
+    respond_to do |format|
+      format.html
+    end
   end
 end
