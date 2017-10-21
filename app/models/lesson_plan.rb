1dfd71c0b1a57848bc78c49fc94c4a5675309d61
Peter Woo <peterw@eff.org>
2017-10-20 17:42:54 -0700

    Removing lessons from lesson plan

--- app/models/lesson_plan.rb
+++ app/models/lesson_plan.rb
@@ -2,5 +2,5 @@ class LessonPlan < ApplicationRecord
   has_many :lesson_plan_lessons
   has_many :lessons, through: :lesson_plan_lessons
 
-  accepts_nested_attributes_for :lesson_plan_lessons
+  accepts_nested_attributes_for :lesson_plan_lessons, allow_destroy: true
 end
