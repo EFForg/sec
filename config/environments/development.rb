225acebc7a478c95dd07290336f65dd5a88c31ee
Peter Woo <peterw@eff.org>
2017-10-23 17:46:56 -0700

    Respect RAILS_RELATIVE_URL_ROOT in url helpers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  if ENV["RAILS_RELATIVE_URL_ROOT"]
+    config.action_controller.default_url_options = {
+      script_name: ENV["RAILS_RELATIVE_URL_ROOT"]
+    }
+  end
 end
