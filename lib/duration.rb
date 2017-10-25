10f8de65d1c5b2614e00bac437b3237e1fd83d79
vivian <vivian@eff.org>
2017-10-25 16:47:13 -0700

    Reorder code in duration class

--- lib/duration.rb
+++ lib/duration.rb
@@ -1,29 +1,5 @@
 class Duration
   include ActionView::Helpers::TextHelper
-
-  # Used for ActiveRecord serialize method
-  def self.load(duration)
-    return nil if duration.nil?
-    self.new(duration)
-  end
-
-  def self.dump(obj)
-    return nil if obj.nil? || empty_duration_hash?(obj)
-    if obj.is_a? Hash
-      self.new(obj).length
-    else
-      obj.length
-    end
-  end
-
-  # Passing empty duration params should set the column to nil, not 0
-  def self.empty_duration_hash?(obj)
-    return false unless obj.is_a?(Hash) 
-    obj.all? do |key, value|
-      value == ""
-    end
-  end
-
   attr_accessor :hours, :minutes
 
   def initialize(duration)
@@ -45,5 +21,28 @@ class Duration
     result.push(pluralize(hours, "hour")) unless hours.zero?
     result.push(pluralize(minutes, "minute")) unless minutes.zero?
     result.join(" and ")
+  end
+
+  # Passing empty duration params should set the column to nil, not 0
+  def self.empty_duration_hash?(obj)
+    return false unless obj.is_a?(Hash)
+    obj.all? do |key, value|
+      value == ""
+    end
+  end
+
+  # Used for ActiveRecord serialize method
+  def self.load(duration)
+    return nil if duration.nil?
+    self.new(duration)
+  end
+
+  def self.dump(obj)
+    return nil if obj.nil? || empty_duration_hash?(obj)
+    if obj.is_a? Hash
+      self.new(obj).length
+    else
+      obj.length
+    end
   end
 end
