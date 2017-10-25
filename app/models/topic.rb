8c1c26df7a1e891f4c3a37b3cfdc67c5e7794321
vivian <vivian@eff.org>
2017-10-25 11:35:37 -0700

    Reject empty duration in nested attributes

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ApplicationRecord
   has_many :lessons
-  accepts_nested_attributes_for :lessons, allow_destroy: true, reject_if: :all_blank_or_empty_multiselect
+  accepts_nested_attributes_for :lessons, allow_destroy: true, reject_if: :deep_all_blank
 
   acts_as_taggable
 
@@ -15,9 +15,10 @@ class Topic < ApplicationRecord
   multisearchable against: %i(name lesson_bodies tag_list),
                   if: :published?
 
-  def all_blank_or_empty_multiselect(attributes)
+  def deep_all_blank(attributes)
     attributes.all? do |key, value|
-      key == "_destory" || value == [""] || value.blank?
+      return true if key == "_destory" || value == [""] || value.blank?
+      all_blank_or_empty_multiselect value if value.is_a? Hash
     end
   end
 
