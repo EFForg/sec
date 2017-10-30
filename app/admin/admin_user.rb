f2a1bd4ff4eb071031342b5bd83831ec49bfde01
Peter Woo <peterw@eff.org>
2017-10-30 12:43:25 -0700

    Add action in admin area to invite users

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -21,7 +21,29 @@ ActiveAdmin.register AdminUser do
       f.input :password
       f.input :password_confirmation
     end
+
     f.actions
   end
 
+  collection_action :new_invitation do
+    @user = AdminUser.new
+  end
+
+  collection_action :invitations, method: :post do
+    @user = AdminUser.invite!(params[:admin_user].permit(:email),
+                              current_admin_user)
+
+    if @user.errors.empty?
+      flash[:success] = "User has been successfully invited."
+      redirect_to admin_admin_users_path
+    else
+      messages = @user.errors.full_messages.map { |msg| msg }.join
+      flash[:error] = "Error: " + messages
+      redirect_to new_invitation_admin_admin_users_path
+    end
+  end
+
+  action_item :invite_user, only: :index do
+    link_to "Invite User", new_invitation_admin_admin_users_path
+  end
 end
--- app/controllers/user_invitations_controller.rb
+++ app/controllers/user_invitations_controller.rb
@@ -1,4 +1,6 @@
 class UserInvitationsController < Devise::InvitationsController
+  include ActiveAdmin::Devise::Controller
+
   protected
 
   alias_method :after_sign_in_path_for, :admin_root_path
