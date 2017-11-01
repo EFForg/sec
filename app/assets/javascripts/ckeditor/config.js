4dae0fd926c0c549f326212b33473ae967879301
vivian <vivian@eff.org>
2017-11-01 15:11:58 -0700

    Add indentation buttons to ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -7,7 +7,7 @@ CKEDITOR.editorConfig = function( config )
   config.toolbar = [
     { name: 'clipboard', items: [ 'PasteFromWord' ] },
     { name: 'styles', items: [ 'Format' ] },
-    { name: 'paragraph', groups: [ 'list', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+    { name: 'paragraph', groups: [ 'list', 'blocks', 'align', 'bidi', 'identation' ], items: [ 'NumberedList', 'BulletedList', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'Indent', 'Outdent' ] },
     { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
     { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
     { name: 'insert', items: [ 'Image', 'HorizontalRule' ] }
