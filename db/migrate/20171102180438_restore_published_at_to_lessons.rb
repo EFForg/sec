f192491fbf1fb1d7ff3fba4796f8bced5839d7e9
Peter Woo <peterw@eff.org>
2017-11-02 13:08:24 -0700

    Fix migration

--- db/migrate/20171102180438_restore_published_at_to_lessons.rb
+++ db/migrate/20171102180438_restore_published_at_to_lessons.rb
@@ -1,21 +1,19 @@
 class RestorePublishedAtToLessons < ActiveRecord::Migration[5.1]
-  def change
-    reversible do |dir|
-      dir.down{ Lesson.where("published_at IS NULL").destroy_all }
-    end
-
+  def up
     add_column :lessons, :published_at, :datetime
 
-    reversible do |dir|
-      dir.up do
-        Topic.find_each do |topic|
-          topic.admin_lessons.update_all(published_at: Time.now)
+    Topic.find_each do |topic|
+      topic.admin_lessons.update_all(published_at: Time.now)
 
-          Lesson::LEVELS.each_key do |level_id|
-            topic.admin_lessons.find_or_create_by!(level_id: level_id)
-          end
-        end
+      Lesson::LEVELS.each_key do |level_id|
+        topic.admin_lessons.find_or_create_by!(level_id: level_id)
       end
     end
+  end
+
+  def down
+    Lesson.where("published_at IS NULL").destroy_all
+
+    remove_column :lessons, :published_at, :datetime
   end
 end
