004c685dcc6ffbdd62e7e8ef5c6da337a0452998
vivian <vivian@eff.org>
2017-11-03 16:06:59 -0700

    Require material name

--- app/models/material.rb
+++ app/models/material.rb
@@ -2,6 +2,8 @@ class Material < ApplicationRecord
   has_many :uploads
   accepts_nested_attributes_for :uploads
 
+  validates_presence_of :name
+
   def first_file
     return if uploads.empty?
     uploads.first.file
