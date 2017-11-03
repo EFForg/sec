edef7920691481d8f2401eec4b17edb5e86ff2e4
vivian <vivian@eff.org>
2017-11-03 15:14:48 -0700

    Add temporary upload fallback images

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -1,13 +1,20 @@
 module UploadsHelper
   def file_preview(file)
     return if file.blank?
+    return image_tag(file.thumbnail.url) if file.thumbnail.url
 
-    if file.thumbnail.url
-      image_tag(file.thumbnail.url)
+    case file.content_type
+    when "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+      image_tag("slideshow.jpg")
+    when "application/vnd.oasis.opendocument.text",
+         "application/msword",
+         "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      image_tag("document.jpg")
     else
-      "Currently uploaded: #{file_name(file)}"
+      image_tag("fallback.jpg")
     end
   end
+
 
   def file_name(file)
     File.basename(file.path)
