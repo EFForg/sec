9af25b20e9ff4d54de9f1d7213cca86b3ed5f891
vivian <vivian@eff.org>
2017-11-03 16:00:24 -0700

    Name upload after fter file

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,4 +1,12 @@
 class Upload < ApplicationRecord
   mount_uploader :file, MaterialsUploader
   belongs_to :material
+
+  before_validation :name_after_file, if: ->(m){ m.name.blank? }
+
+  def name_after_file
+    if file.present?
+      self.name = File.basename(file.path)
+    end
+  end
 end
