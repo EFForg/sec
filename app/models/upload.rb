4fd86696ac00245235c7bbe53a30b22cc65fa8d8
vivian <vivian@eff.org>
2017-11-03 16:11:24 -0700

    Order uploads by position

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,6 +2,8 @@ class Upload < ApplicationRecord
   mount_uploader :file, MaterialsUploader
   belongs_to :material
 
+  default_scope { order(position: :asc) }
+
   before_validation :name_after_file, if: ->(m){ m.name.blank? }
 
   def name_after_file
