2ffe016cc4faa7ca8768cf5e474afb7d023739b0
vivian <vivian@eff.org>
2017-11-03 15:24:23 -0700

    Put file preview on materials index

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,4 +1,9 @@
 class Material < ApplicationRecord
   has_many :uploads
   accepts_nested_attributes_for :uploads
+
+  def first_file
+    return if uploads.empty?
+    uploads.first.file
+  end
 end
