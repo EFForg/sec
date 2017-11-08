79e71a0c8c80cb36ffa6da735191be9130c66a36
Peter Woo <peterw@eff.org>
2017-11-07 16:55:32 -0800

    Add table of contents to articles overview. Fixes #210. Fixes #208.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,4 +2,13 @@ module ArticlesHelper
   def cache_key_for_articles(sections)
     [ArticleSection.all.cache_key, Article.all.cache_key]
   end
+
+  def section_name(section)
+    section.try(:name) || "Other Articles"
+  end
+
+  def section_id(section)
+    name = section.try(:name) || "Other Articles"
+    name.gsub(/\s+/, "_")
+  end
 end
