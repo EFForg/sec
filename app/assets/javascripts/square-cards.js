be8dfd7ae325be09c88339d5bd397ae1c227d1e6
Schatzkin <schatzkin@eff.org>
2017-11-10 13:43:10 -0800

    js to square materials cards

---
+++ app/assets/javascripts/square-cards.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  if ($('body').hasClass('section-home')) {
+    var $cardSquare = $('div.topic.card');
+  } else if ($('body').hasClass('section-materials')) {
+    var $cardSquare = $('div.material.card');
+  }
+  function cardResize() {
+    var cardWidth= $cardSquare.width();
+    $cardSquare.css("height", cardWidth);
+  }
+  cardResize();
+  $( window ).resize(function() {
+    cardResize();
+  });
+
+});
