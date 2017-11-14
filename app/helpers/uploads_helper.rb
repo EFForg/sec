eae928366f903d5bb99d6e4cac8254483e6af3ea
vivian <vivian@eff.org>
2017-11-14 14:57:46 -0800

    Display file extension as materials preview fallback

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -3,15 +3,8 @@ module UploadsHelper
     return if file.blank?
     return image_tag(file.thumbnail.url) if file.thumbnail.url
 
-    case file.content_type
-    when "application/vnd.openxmlformats-officedocument.presentationml.presentation"
-      image_tag("previews/slideshow.jpg")
-    when "application/vnd.oasis.opendocument.text",
-         "application/msword",
-         "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-      image_tag("previews/document.jpg")
-    else
-      image_tag("previews/fallback.jpg")
+    content_tag :div, class: "file-preview-fallback" do
+      content_tag(:span, ".#{file.file.extension.downcase}")
     end
   end
 
