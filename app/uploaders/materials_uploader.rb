40874d933dd6c73c76f9332d0405488c5af3e966
vivian <vivian@eff.org>
2017-11-16 21:00:15 -0800

    Make private uploader methods private

--- app/uploaders/materials_uploader.rb
+++ app/uploaders/materials_uploader.rb
@@ -11,42 +11,6 @@ class MaterialsUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def gif_safe_transform!
-    MiniMagick::Tool::Convert.new do |image|
-      image << @file.path
-      image.coalesce
-
-      yield image
-
-      image.layers "Optimize"
-      image << @file.path
-    end
-  end
-
-  def imagemagick_do_crop(image, size, orig_width, orig_height)
-    if orig_width < orig_height
-      remove = ((orig_height - orig_width)/2).round
-      image.shave("0x#{remove}")
-    elsif orig_width > orig_height
-      remove = ((orig_width - orig_height)/2).round
-      image.shave("#{remove}x0")
-    end
-    image.resize("#{size}x#{size}")
-  end
-
-  def crop_square(size)
-    if @file.content_type == "image/gif"
-      original = MiniMagick::Image.new(@file.path)
-      gif_safe_transform! do |image|
-        imagemagick_do_crop(image, size, original.width, original.height)
-      end
-    else
-      manipulate! do |image|
-        imagemagick_do_crop(image, size, image.width, image.height)
-      end
-    end
-  end
-
   version :full_preview, if: :is_previewable? do
     def full_filename(filename = model.source.file)
       "preview_#{filename.sub(/\.pdf\z/, ".png")}"
@@ -69,5 +33,41 @@ class MaterialsUploader < CarrierWave::Uploader::Base
 
   def is_previewable?(file)
     content_type.include?("image") || is_pdf?(file)
+  end
+
+  def gif_safe_transform!
+    MiniMagick::Tool::Convert.new do |image|
+      image << @file.path
+      image.coalesce
+
+      yield image
+
+      image.layers "Optimize"
+      image << @file.path
+    end
+  end
+
+  def crop_square(size)
+    if @file.content_type == "image/gif"
+      original = MiniMagick::Image.new(@file.path)
+      gif_safe_transform! do |image|
+        imagemagick_do_crop(image, size, original.width, original.height)
+      end
+    else
+      manipulate! do |image|
+        imagemagick_do_crop(image, size, image.width, image.height)
+      end
+    end
+  end
+
+  def imagemagick_do_crop(image, size, orig_width, orig_height)
+    if orig_width < orig_height
+      remove = ((orig_height - orig_width)/2).round
+      image.shave("0x#{remove}")
+    elsif orig_width > orig_height
+      remove = ((orig_width - orig_height)/2).round
+      image.shave("#{remove}x0")
+    end
+    image.resize("#{size}x#{size}")
   end
 end
