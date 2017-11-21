80b35c4e814ebe9d5df29931802f7f4e97de6812
Peter Woo <peterw@eff.org>
2017-11-21 09:43:37 -0800

    Add facebook/twitter/google share links to side of page

---
+++ app/helpers/share_link_helper.rb
@@ -0,0 +1,25 @@
+
+module ShareLinkHelper
+  def share_link(type)
+    link_to send("#{type}_share_url"), class: type.to_s, target: "_blank" do
+      content_tag(:span, class: "sr-only"){ type.to_s.capitalize }
+    end
+  end
+
+  def twitter_share_url
+    status = "#{page_title}\n\n#{url_for_share}"
+    "https://twitter.com/intent/tweet?status=#{u(status)}"
+  end
+
+  def google_share_url
+    "https://plus.google.com/share?url=#{u(url_for_share)}"
+  end
+
+  def facebook_share_url
+    "https://www.facebook.com/sharer/sharer.php?u=#{u(url_for_share)}&display=popup"
+  end
+
+  def url_for_share
+    request.original_url
+  end
+end
