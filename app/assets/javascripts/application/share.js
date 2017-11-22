d4448abf35e4276e643a4267f886fd5a63bd3614
Peter Woo <peterw@eff.org>
2017-11-22 11:45:32 -0800

    Open share links in popup

---
+++ app/assets/javascripts/application/share.js
@@ -0,0 +1,19 @@
+
+$(document).on("click", ".share-links-sidebar .facebook", function(e) {
+  var url = $(this).attr("href");
+  window.open(url, "Share on Facebook", "width=650,height=500");
+  e.preventDefault();
+});
+
+$(document).on("click", ".share-links-sidebar .twitter", function(e) {
+  var url = $(this).attr("href");
+  window.open(url, "Share on Twitter", "width=550,height=420");
+  e.preventDefault();
+});
+
+$(document).on("click", ".share-links-sidebar .google", function(e) {
+  var url = $(this).attr("href");
+  window.open(url, "Share on Google+", "width=500,height=436");
+  e.preventDefault();
+});
+
