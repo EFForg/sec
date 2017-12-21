e7cfbe910cfda6b64e54da3315563324dbd6a622
Peter Woo <peterw@eff.org>
2017-12-21 11:43:02 -0800

    Use current page title/description/url for og tags

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -3,14 +3,14 @@ module ContentHelper
     %w(b strong i em u s strike del p)
   end
 
-  def preview(object)
+  def preview(object, allowed_tags: allowed_tags_for_preview)
     if object.respond_to?(:summary?) && object.summary?
       html = object.summary
     else
       html = object.body
     end
 
-    html = truncate(sanitize(html, tags: allowed_tags_for_preview),
+    html = truncate(sanitize(html, tags: allowed_tags),
                     length: 500, escape: false, separator: /\s/)
 
     Nokogiri::HTML.fragment(html).to_html.html_safe # rubocop:disable Rails/OutputSafety
