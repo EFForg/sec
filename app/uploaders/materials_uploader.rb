a14584354858990924c8d25210e85771fdcc098a
Peter Woo <peterw@eff.org>
2018-02-08 16:53:11 -0800

    Use a separate storage dir for test uploads

--- app/uploaders/materials_uploader.rb
+++ app/uploaders/materials_uploader.rb
@@ -8,7 +8,8 @@ class MaterialsUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    base = Rails.env.test? ? "/tmp/sec-test" : "uploads/"
+    "#{base}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   version :full_preview, if: :is_previewable? do
