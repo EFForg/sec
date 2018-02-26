85a3e6cf2fa0172316754f083fe5c38a718a64ec
Peter Woo <peterw@eff.org>
2018-02-26 14:56:19 -0800

    Add admin ui to easily review survey question responses

--- app/admin/survey_question.rb
+++ app/admin/survey_question.rb
@@ -6,6 +6,17 @@ ActiveAdmin.register SurveyQuestion do
   permit_params :prompt, :required,
                 options_attributes: [:id, :position, :value]
 
+  index do
+    id_column
+
+    column :prompt
+    column :required
+
+    actions do |resource|
+      link_to "Show Responses", show_responses_admin_survey_question_path(resource)
+    end
+  end
+
   form do |f|
     f.inputs do
       f.input :prompt
@@ -18,5 +29,10 @@ ActiveAdmin.register SurveyQuestion do
     end
 
     f.actions
+  end
+
+  member_action :show_responses do
+    @survey_question = resource
+    render layout: "active_admin"
   end
 end
--- app/models/survey_question.rb
+++ app/models/survey_question.rb
@@ -1,4 +1,6 @@
 class SurveyQuestion < ApplicationRecord
   has_many :options, class_name: "SurveyOption", dependent: :destroy
   accepts_nested_attributes_for :options
+
+  has_many :responses, class_name: "SurveyResponse"
 end
