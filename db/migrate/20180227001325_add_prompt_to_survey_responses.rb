481e161d16748491ad47af5c564f33527524fc23
Peter Woo <peterw@eff.org>
2018-02-26 16:26:03 -0800

    Fix migration

--- db/migrate/20180227001325_add_prompt_to_survey_responses.rb
+++ db/migrate/20180227001325_add_prompt_to_survey_responses.rb
@@ -5,7 +5,7 @@ class AddPromptToSurveyResponses < ActiveRecord::Migration[5.1]
     reversible do |dir|
       dir.up do
         SurveyResponse.all.find_each do |response|
-          response.prompt ||= response.survey_question.prompt
+          response.prompt = response.survey_question.prompt
           response.save
         end
       end
