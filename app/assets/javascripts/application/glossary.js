54210e0a52aee360bbe3f53a5f4e1520dcce963b
K. Stewart <ignitiant@gmail.com>
2018-03-13 12:55:08 -0700

    Better glossary tooltips
    * Close old tooltips when opening a new one

--- app/assets/javascripts/application/glossary.js
+++ app/assets/javascripts/application/glossary.js
@@ -1,26 +1,16 @@
-$(document).on('mouseover', '.glossary-term', showDefinition);
+$(document).on('mouseover', '.glossary-term', toggleDefinition);
 $(document).on("click", ".glossary-term", toggleDefinition);
 $(document).click(function(e) {
   if (!$(e.target).closest(".glossary-term").length) {
-    hideDefinitions();
+    $(".glossary-definition").hide("fast");
   };
 });
 
-function hideDefinitions() {
-  $(".glossary-definition").hide("fast");
-}
-
 function toggleDefinition(e) {
   if(e.target == e.currentTarget) {
-    $("#"+getDropdownId(e)).toggle("fast");
+    id = $(e.target).data()["toggle"];
+
+    $(".glossary-definition").not(e.currentTarget).hide("fast");
+    $("#"+id).toggle("fast");
   }
 };
-
-function showDefinition(e) {
-  hideDefinitions();
-  $("#"+getDropdownId(e)).show("fast");
-};
-
-function getDropdownId(e) {
-  return $(e.target).data()["toggle"];
-}
