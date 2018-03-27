aef7d7b5bf549a17f2845de9cf47d013ece55df2
K. Stewart <ignitiant@gmail.com>
2018-03-27 11:51:08 -0700

    Open and close glossary tooltips with mouseover
    Retain click functionality for touchscreens.
    Also, use single quotes

--- app/assets/javascripts/application/glossary.js
+++ app/assets/javascripts/application/glossary.js
@@ -1,16 +1,31 @@
-$(document).on('mouseover', '.glossary-term', toggleDefinition);
-$(document).on("click", ".glossary-term", toggleDefinition);
-$(document).click(function(e) {
-  if (!$(e.target).closest(".glossary-term").length) {
-    $(".glossary-definition").hide("fast");
+$(document).on('mouseover', '.glossary-term, .glossary-definition', (e) => {
+  var def = $(e.target).closest('.glossary-term').find('.glossary-definition');
+  clearTimeout(def.data('timeout'));
+  def.data('timeout', null).show();
+});
+
+$(document).on('mouseout', '.glossary-term, .glossary-definition', (e) => {
+  var def = $(e.target).closest('.glossary-term').find('.glossary-definition');
+  if (!def.data('timeout')) {
+    def.data('timeout', setTimeout(() => {
+      def.data('timeout', null).hide();
+    }, 200));
+  }
+});
+
+$(document).on('click', '.glossary-term', (e) => {
+  if(e.target == e.currentTarget) {
+    id = $(e.target).data()['toggle'];
+
+    $('.glossary-definition').not(e.currentTarget).hide('fast');
+    $('#'+id).toggle('fast');
+  }
+});
+
+$(document).click((e) => {
+  if (!$(e.target).closest('.glossary-term').length) {
+    $('.glossary-definition').hide('fast');
   };
 });
 
-function toggleDefinition(e) {
-  if(e.target == e.currentTarget) {
-    id = $(e.target).data()["toggle"];
 
-    $(".glossary-definition").not(e.currentTarget).hide("fast");
-    $("#"+id).toggle("fast");
-  }
-};
