1ab4de69b653fc2da7bc339429d9cbb954887023
Vivian Brown <vivian@eff.org>
2018-03-30 16:22:22 -0700

    Move articles page to models

--- app/admin/articles_overview.rb
+++ app/admin/articles_overview.rb
@@ -55,10 +55,4 @@ ActiveAdmin.register_page "Articles Overview" do
     render "admin/pages/articles_overview",
       page: ArticlesPage.find_by!(name: "articles-overview")
   end
-
-  class ArticlesPage < Page
-    def sections
-      @sections ||= ArticleSection.order(:position)
-    end
-  end
 end
