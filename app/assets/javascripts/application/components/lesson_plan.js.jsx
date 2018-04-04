48be22ae3831a976ecab6ce4124bf7a2ec8f518e
Vivian Brown <vivian@eff.org>
2018-04-04 12:11:09 -0700

    Remove lesson from dom

--- app/assets/javascripts/application/components/lesson_plan.js.jsx
+++ app/assets/javascripts/application/components/lesson_plan.js.jsx
@@ -5,37 +5,62 @@ $(window).on("load", function() {
   lessons.sortable({
     // handle: ".handle",
     axis: "y",
-    stop: updatePositions
+    // stop: updatePositions
   });
 
-  function updatePositions() {
-    lessons.each(function(i) {
-      $(this).find("input[name*=position]").val(i);
-    });
-  }
+  // function updatePositions() {
+  //   lessons.each(function(i) {
+  //     $(this).find("input[name*=position]").val(i);
+  //   });
+  // }
 });
 
 var LessonPlan = createReactClass({
+  getInitialState: function() {
+    return {
+      lessonsCount: this.props.lessons_count,
+      durationInWords: this.props.duration_in_words,
+      lessons: this.props.lessons
+    };
+  },
+
+  componentDidMount: function() {
+
+  },
+
+  componentWillUnmount: function() {
+
+  },
+
+  onRemoveLesson: function(e) {
+    var state = this.state;
+    this.setState({
+      lessonsCount: state.lessonsCount - 1,
+      lessons: state.lessons.filter(el => el.id.toString() !== e.target.id)
+    });
+  },
+
   render: function() {
     var props = this.props;
-
-    console.log(props);
+    var state = this.state;
 
     return (
       <div className="lesson-plan">
         <div className="your-lessons">
-          Your lessons (<span id="lesson-count">{props.lessons_count}</span>)
+          Your lessons (<span id="lesson-count">{state.lessonsCount}</span>)
         </div>
         <div className="total-duration">
           Total duration: {props.duration_in_words}
         </div>
         <ul id="lesson-plan-lessons">
-          {props.lessons.map((lesson) =>
+          {state.lessons.map((lesson) =>
             <Lesson key={lesson.id}
+                    id={lesson.id}
                     name={lesson.name}
                     duration={lesson.duration}
                     difficultyTag={lesson.difficulty_tag}
-                    renderedIcon={lesson.rendered_icon} />
+                    renderedIcon={lesson.rendered_icon}
+                    onRemoveLesson={this.onRemoveLesson} />
           )}
         </ul>
       </div>
@@ -54,6 +79,7 @@ var Lesson = createReactClass({
           <h3>{props.name}</h3>
           <div className="duration">Duration: {props.duration}</div>
           <div className="levels" dangerouslySetInnerHTML={{__html: props.difficultyTag}} />
+          <button id={props.id} onClick={this.props.onRemoveLesson}>Remove</button>
         </div>
       </li>
     );
