9fb443a3129c2cc2aa9c8c178cb48a35b367fcdf
Peter Woo <peterw@eff.org>
2018-04-04 12:46:51 -0700

    Initialize lesson plan counts from javascript

--- app/assets/javascripts/application/lessons.js
+++ app/assets/javascripts/application/lessons.js
@@ -8,3 +8,20 @@ $(document).on('click', '.expand-collapsed', function(e) {
     history.replaceState({}, '', this.href);
   }
 });
+
+$(document).on('turbolinks:load ajax:complete', function(e) {
+  if (!window.session)
+    return;
+
+  var lessonPlanLessons = window.session.lessonPlanLessons;
+
+  $('#lesson-count, .lesson-count')
+    .html('(' + lessonPlanLessons.length + ')');
+
+  $('form.add_remove_lesson').each(function() {
+    if (lessonPlanLessons.indexOf($(this).data('lesson-id')) != -1)
+      $('input[name=_method]', this).attr('value', 'delete');
+    else
+      $('input[name=_method]', this).attr('value', 'post');
+  });
+});
