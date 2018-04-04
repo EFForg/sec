137a4f2478a5c28b3cd6d37bf34fcbecf9284ed6
Peter Woo <peterw@eff.org>
2018-04-04 11:58:42 -0700

    Fix lesson plan feature spec

--- spec/features/add_to_lesson_plans_spec.rb
+++ spec/features/add_to_lesson_plans_spec.rb
@@ -8,11 +8,14 @@ RSpec.feature "AddToLessonPlans", type: :feature do
   scenario "user adds a lesson to a new lesson plan" do
     visit topic_path(topic)
 
-    click_button "Add To Lesson Plan (0)"
+    button = click_button "Add To Lesson Plan"
+    expect(button.text).to include("(0)")
 
     expect(LessonPlan.count).to eq(1)
     expect(LessonPlan.take.lesson_ids).to eq([lesson.id])
-    find_button("Remove From Lesson Plan (1)")
+
+    button = find_button("Remove From Lesson Plan")
+    expect(button.text).to include("(1)")
   end
 
   scenario "user adds a lesson to an existing lesson plan" do
@@ -21,7 +24,7 @@ RSpec.feature "AddToLessonPlans", type: :feature do
       LessonPlan.find(lesson_plan.id)
     end
     visit topic_path(topic)
-    click_button "Add To Lesson Plan (1)"
+    click_button "Add To Lesson Plan"
     expect(lesson_plan.lessons.count).to eq(2)
   end
 
@@ -31,7 +34,8 @@ RSpec.feature "AddToLessonPlans", type: :feature do
       LessonPlan.find(lesson_plan.id)
     end
     visit topic_path(topic)
-    click_button "Remove From Lesson Plan (1)"
+    click_button "Remove From Lesson Plan"
     expect(lesson_plan.lessons.count).to eq(0)
+    find_button "Add To Lesson Plan"
   end
 end
