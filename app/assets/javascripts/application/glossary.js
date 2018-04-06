21948995f91e844624b569ac1ed6c73949747b36
Peter Woo <peterw@eff.org>
2018-04-06 09:01:07 -0700

    Remove ES6 syntax not supported by capybara-webkit

--- app/assets/javascripts/application/glossary.js
+++ app/assets/javascripts/application/glossary.js
@@ -1,18 +1,18 @@
-$(document).on('mouseover', '.glossary-term, .glossary-definition', (e) => {
+$(document).on('mouseover', '.glossary-term, .glossary-definition', function(e) {
   var def = $(e.target).closest('.glossary-term').find('.glossary-definition');
   clearTimeout(def.data('timeout'));
   def.data('timeout', null).show();
 });
 
-$(document).on('mouseout', '.glossary-term, .glossary-definition', (e) => {
+$(document).on('mouseout', '.glossary-term, .glossary-definition', function(e) {
   var def = $(e.target).closest('.glossary-term').find('.glossary-definition');
   if (!def.data('timeout')) {
-    def.data('timeout', setTimeout(() => {
+    def.data('timeout', setTimeout(function() {
       def.data('timeout', null).hide();
     }, 200));
   }
 });
 
-$(document).on('click', '.glossary-term', (e) => {
+$(document).on('click', '.glossary-term', function(e) {
   window.open($(e.target).find('a').attr('href'));
 });
