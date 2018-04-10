6b2e12a6e4357cdd82a6160c9950db82b1485f93
Vivian Brown <vivian@eff.org>
2018-04-10 13:58:42 -0700

    Fix manage lesson plan test

--- spec/features/manage_lesson_plan_spec.rb
+++ spec/features/manage_lesson_plan_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "ManageLessonPlan", type: :feature do
-  let(:topic){ FactoryGirl.create(:topic) }
-  let(:lesson){ topic.lessons.first }
+RSpec.feature "ManageLessonPlan", type: :feature, js: true do
   let(:lesson_plan) { FactoryGirl.create(:lesson_plan_with_lesson) }
 
   scenario "user removes a lesson from their lesson plan" do
@@ -11,6 +9,7 @@ RSpec.feature "ManageLessonPlan", type: :feature do
     visit "/lesson-plan"
     click_button "Remove this lesson"
 
+    expect(page).to have_no_content("A topic")
     expect(lesson_plan.lessons.count).to eq(0)
   end
 end
