6271e71f6cdabec1353e369fa74902097217f53a
vivian <vivian@eff.org>
2018-05-11 14:07:49 -0700

    Fix Ruby lint

--- app/helpers/lesson_plans_helper.rb
+++ app/helpers/lesson_plans_helper.rb
@@ -1,9 +1,9 @@
 module LessonPlansHelper
-  def lesson_plan_share_url!(lesson_plan, ext=nil)
+  def lesson_plan_share_url!(lesson_plan, ext = nil)
     request.base_url + lesson_plan_share_path!(lesson_plan, ext)
   end
 
-  def lesson_plan_share_path!(lesson_plan, ext=nil)
+  def lesson_plan_share_path!(lesson_plan, ext = nil)
     "#{lesson_plans_path}/#{lesson_plan.key!}#{ext}"
   end
 end
