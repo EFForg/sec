7199fa0a27f8e309931a68a4beb062cc29b42df6
Peter Woo <peterw@eff.org>
2019-07-17 13:09:59 -0700

    Disable chrome's w3c mode

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,6 +39,7 @@ capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     server: "ALL"
   },
   'chromeOptions' => {
+    'w3c', => false,
     'args' => ['--headless', '--disable-gpu'].tap do |a|
       a.push('--no-sandbox') if ENV['TRAVIS']
     end
