7e9551965a0ea1b088fb42150fd2e9053dcd8ad2
Peter Woo <peterw@eff.org>
2019-12-02 14:15:32 -0800

    Clean up rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-require 'capybara/rspec'
 require 'rack_session_access/capybara'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -29,7 +28,6 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-require "capybara/rspec"
 
 capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
   loggingPrefs: {
