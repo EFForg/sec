b1cdcdcc2d0cc31aa1f988c7c06970fa008148e4
Peter Woo <peterw@eff.org>
2020-01-30 10:36:07 -0800

    Allow admins to export entire site to markdown

---
+++ app/models/markdown_archive.rb
@@ -0,0 +1,66 @@
+class MarkdownArchive
+  attr_accessor :articles, :topics
+
+  def initialize(articles: Article.all, topics: Topic.all)
+    self.articles = articles
+    self.topics = topics
+  end
+
+  def zip
+    Dir.mktmpdir do |tmp|
+      tmp = Pathname.new(tmp)
+
+      articles.each do |article|
+        add_article(tmp, article)
+      end
+
+      topics.each do |topic|
+        topic.lessons.each do |lesson|
+          add_lesson(tmp, lesson)
+        end
+      end
+
+      zip = Tempfile.new(['archive', '.zip'])
+      File.delete(zip.to_path)
+
+      system(
+        'sh', '-c',
+        "cd #{tmp.to_s} && zip -r a.zip . && mv a.zip #{zip.to_path}"
+      )
+
+      return zip.to_path
+    end
+  end
+
+  private
+
+  def add_article(tmp, article)
+    doc = ArticlesController.render(
+      template: "articles/show.md.erb",
+      assigns: { article: article }
+    )
+
+    path = [
+      "Articles",
+      article.name.strip.gsub("/", "-") + ".md"
+    ].join("/")
+
+    FileUtils.mkdir_p(File.dirname(tmp.join(path)))
+    File.open(tmp.join(path), 'w'){ |f| f.write(doc) }
+  end
+
+  def add_lesson(tmp, lesson)
+    doc = TopicsController.render(
+      template: "lessons/show.md.erb",
+      assigns: { topic: lesson.topic, lesson: lesson }
+    )
+
+    path = [
+      "Lessons",
+      lesson.topic.name.strip.gsub("/", "-") + " - #{lesson.level}.md"
+    ].join("/")
+
+    FileUtils.mkdir_p(File.dirname(tmp.join(path)))
+    File.open(tmp.join(path), 'w'){ |f| f.write(doc) }
+  end
+end
