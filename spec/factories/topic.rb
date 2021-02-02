722ccb99d065e17a93a18209765df89ab4c9d3af
brittany <brittany@eff.org>
2021-02-01 16:19:01 -0800

    490: add and remove all lessons in a topic button feature tests

--- spec/factories/topic.rb
+++ spec/factories/topic.rb
@@ -2,5 +2,10 @@ FactoryGirl.define do
   factory :topic do
     name "A topic"
     published true
+
+    factory :topic_with_lessons do
+      name "Another topic"
+      description "Any old description to make sure we land on the Intro tab"
+    end
   end
 end
